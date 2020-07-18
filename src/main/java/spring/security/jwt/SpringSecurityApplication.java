8bcc8ad26e42696c0e971527edf628def8faecd0
Star Zheng <zhengyifan1996@outlook.com>
2020-07-18 14:27:31 +0800

    Update SpringSecurityApplication.java

--- src/main/java/spring/security/jwt/SpringSecurityApplication.java
+++ src/main/java/spring/security/jwt/SpringSecurityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @SpringBootApplication
 public class SpringSecurityApplication {
 
