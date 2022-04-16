39e08a0a5d469e30b9248b67ee91618ea98a7aee
Star Zheng <zhengyifan1996@outlook.com>
2022-04-16 22:03:24 +0800

    Update SpringSecurityApplication.java

--- src/main/java/spring/security/jwt/SpringSecurityApplication.java
+++ src/main/java/spring/security/jwt/SpringSecurityApplication.java
@@ -10,7 +10,32 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 public class SpringSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringSecurityApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback.");
+        }
+
+        log.info("\n----------------------------------------------------------\n\t"
+                    + "Application '{}' is running! Access URLs:\n\t" 
+                    + "Local: \t\t{}://localhost:{}\n\t"
+                    + "External: \t{}://{}:{}\n\t"
+                    + "Profile(s): \t{}"
+                    + "\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"), 
+                protocol, 
+                env.getProperty("server.port"), 
+                protocol,
+                hostAddress, 
+                env.getProperty("server.port"), 
+                env.getActiveProfiles());
+    }
 }
