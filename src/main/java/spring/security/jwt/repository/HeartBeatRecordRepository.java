896940412a1b0c084e32ed27bb5c0b82a606dbb6
Star Zheng <zhengyifan1996@outlook.com>
2022-10-10 12:10:30 +0800

    format

--- src/main/java/spring/security/jwt/repository/HeartBeatRecordRepository.java
+++ src/main/java/spring/security/jwt/repository/HeartBeatRecordRepository.java
@@ -1,7 +1,5 @@
 package spring.security.jwt.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import spring.security.jwt.entity.HeartBeatRecord;
